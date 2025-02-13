4b876648a31b1c1c00960d88ab0d04e79c742091
cristian <cristianandres1496@gmail.com>
2025-02-12 22:20:10 -0500

    Prepare for Railway

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -5,8 +5,8 @@ from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
-def home_page_view(request,*args,**kwargs):
-    qs = PageVisit.objects.all()
+def home_view(request,*args,**kwargs):
+    """ qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
     my_title = "My page"
     html_template = "home.html"
@@ -14,6 +14,31 @@ def home_page_view(request,*args,**kwargs):
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
+        "percent":(page_qs.count() * 100) / qs.count(),
+        "total_visit_count":qs.count()
+    }
+
+    PageVisit.objects.create(path=request.path)
+    
+    return render(request,html_template,context=my_context) """
+    return render(request,*args,**kwargs)
+
+
+def about_view(request,*args,**kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100) / qs.count(),
+    except:
+        percent = 0
+
+    my_title = "My page"
+    html_template = "home.html"
+    
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "percent":percent,
         "total_visit_count":qs.count()
     }
 
